873a12e52f1ccd5190d6383b88cce7d8e67dd911
Troy Liu <troyliu0105@outlook.com>
2019-11-16 20:04:59 +0800

    UPADATE: try fix boost

--- boost.rb
+++ boost.rb
@@ -53,9 +53,9 @@ class Boost < Formula
     # Boost is using "clang++ -x c" to select C compiler which breaks C++14
     # handling using ENV.cxx14. Using "cxxflags" and "linkflags" still works.
     args << "cxxflags=-std=c++11"
-    if ENV.compiler == :clang
-      args << "cxxflags=-stdlib=libc++" << "linkflags=-stdlib=libc++"
-    end
+    # if ENV.compiler == :clang
+    #   args << "cxxflags=-stdlib=libc++" << "linkflags=-stdlib=libc++"
+    # end
 
     system "./bootstrap.sh", *bootstrap_args
     system "./b2", "headers"
